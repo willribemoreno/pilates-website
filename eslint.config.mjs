a29cb653415d4f2c08d829e0d2f1749494cae89d
William Moreno <william.ribeiro.moreno@gmail.com>
2025-03-12 11:35:51 -0300

    feat: Add Auth

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["**/.next/**"], // Exclude the .next folder
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
