e530f9157c31b2da52a63e14a0935c428f8eedd1
William Moreno <william.ribeiro.moreno@gmail.com>
2025-03-12 11:53:51 -0300

    feat: Add seed methods for handling database

---
+++ app/seed/route.ts
@@ -0,0 +1,42 @@
+import bcrypt from 'bcryptjs';
+import postgres from 'postgres';
+import { users } from '../lib/placeholder-data';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+async function seedUsers() {
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await sql`
+    CREATE TABLE IF NOT EXISTS users (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL
+    );
+  `;
+
+  const insertedUsers = await Promise.all(
+    users.map(async (user) => {
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+      return sql`
+        INSERT INTO users (id, name, email, password)
+        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+    }),
+  );
+
+  return insertedUsers;
+}
+
+export async function GET() {
+  try {
+    const result = await sql.begin((sql) => [
+      seedUsers(),
+    ]);
+
+    return Response.json({ message: 'Database seeded successfully' });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
