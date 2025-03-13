bacc8b1188fbc0ff72665e0c9f988fe76e757b2a
William Moreno <william.ribeiro.moreno@gmail.com>
2025-03-13 10:58:07 -0300

    feat: Add draft for Manage page + update gitignore file

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,3 +1,30 @@
+import Navbar from "@/app/ui/shared/navbar/navbar";
+import Footer from "@/app/ui/home/footer";
+
+const footerProps = {
+  rights: "2025 Pilates Studio. Todos os direitos reservados.",
+};
+
+// { name: "Sobre", href: "#", current: false },
+const navbarProps = {
+  navigation: [
+    { name: "Início", href: "#", current: true },
+    { name: "Aulas", href: "#about", current: false },
+    { name: "Contato", href: "#contact", current: false },
+  ],
+  menuItems: [
+    { name: "Meu Perfil", href: "#" },
+    { name: "Configurações", href: "#" },
+    { name: "Deslogar", href: "#" },
+  ],
+};
+
 export default function Manage() {
-  return <h1>Manage Section</h1>;
+  return (
+    <div className="flex flex-col h-screen">
+      <Navbar {...navbarProps} />
+      <h1 className="flex flex-grow items-center justify-center text-2xl">Manage Section</h1>
+      <Footer {...footerProps} />
+    </div>
+  );
 }
